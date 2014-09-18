5f97767f87018002cc8db4e6e9d23567fd2a1442
Spencer Fornaciari <mail@spencerfornaciari.com>
2014-09-17 21:23:37 -0700

    Rewriting Film Heat in Swift

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Film Heat
+//
+//  Created by Spencer Fornaciari on 9/17/14.
+//  Copyright (c) 2014 Azzurri Ventures. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
